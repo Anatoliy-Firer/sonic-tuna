335a60f42df43bf240c96cd75edc9bfa92002be8
Анатолий Фирер <amfirer1@mts.ru>
2026-03-30 07:14:02 +0300

    - Теперь скрипт постоянно сидит в конфе и динамически связывается со всеми собеседниками в канале - Убран OpenCV (для отладки можно использовать `headless=False`)

--- check_cams.js
+++
@@ -1,9 +0,0 @@
-const videos = document.getElementsByTagName("video");
-if(videos.length < 2) return false;
-for(var i = 1; i < videos.length; ++i) {
-    const src = videos[i].srcObject
-    if(src != null && src instanceof MediaStream && src.active){
-        return true;
-    }
-}
-return false;
