60432aeef26cb3ddab2cde08f53771314cc25d5c
Анатолий Фирер <amfirer1@mts.ru>
2026-04-19 21:13:46 +0300

    Двойная отправка кадра

--- src/camera_bridge.js
+++ src/camera_bridge.js
@@ -49,10 +49,10 @@ function drawFrame(grayBytes) {
     sourceCtx.putImageData(imageData, 0, 0);
     ctx.drawImage(sourceCanvas, 0, 0, WIDTH, HEIGHT);
     ctx.fillStyle = '#000'; // двойная отправка кадра, чтобы сформировать на принимающей стороне более устойчивое изображение
-    sourceCtx.fillRect(0,0,8,8)
+    sourceCtx.fillRect(0,0,8,8);
     videoTrack.requestFrame?.();
     ctx.fillStyle = '#fff';
-    sourceCtx.fillRect(0,0,8,8)
+    sourceCtx.fillRect(0,0,8,8);
     videoTrack.requestFrame?.();
 }
 
